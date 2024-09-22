53fde87fd818fb4533a12b348af087a260d3581b
Fouzia ifat <fouzia786ifat@gmail.com>
2024-09-23 02:18:25 +0530

    commit change

--- script.js
+++ script.js
@@ -1,34 +1,44 @@
-let cardNow = [];
+let cart = [];
 let cartBadge = document.querySelector(".cart-badge");
 let cartPopup = document.querySelector("#cart-popup");
 let cartItem = document.querySelector("#cart-items");
 let total = document.querySelector("#total-price");
+let closeCart = document.querySelector(".close-cart")
 
-function addTocard (item ,price){
-    const foundItem = cardNow.find((cartItem) =>cartItem.name === item)
+function addTocard (item ,price, image){
+    const foundItem = cart.find((cartItem) =>cartItem.name === item)
     if(foundItem){
         foundItem.quantity += 1
     } else {
-        cardNow.push({name: item, price: price, quantity: 1})
+        cart.push({name: item, price: price, quantity: 1, image: image})
     }
     updateCartItem()
    updateCartBbadge() 
 }
 function updateCartBbadge(){
-    let totalQuantity = cardNow.reduce((total, item) => total + item.quantity, 0)
+    let totalQuantity = cart.reduce((total, item) => total + item.quantity, 0)
     cartBadge.textContent = totalQuantity
-    console.log(cardNow);   
+    console.log(cart);   
 };
+
+
+closeCart.addEventListener('click', function(){
+    alert ("Cart is closing");
+    window.location.href = '/';
+})
+
+
 function toggleCart(){
  cartPopup.classList.toggle("show-cart")
 }
+
 function updateCartItem() {
     cartItem.innerHTML = "" ;
-    cardNow.forEach((item, index) => {
+    cart.forEach((item, index) => {
         const itemCart = document.createElement("div");
         itemCart.classList.add("cart-item-card");
         itemCart.innerHTML = `
-        <img src="/images/img1.jpg" alt=${item.name} class="cart-item-image">
+        <img src="${item.image}" alt=${item.name} class="cart-item-image">
         <div class="cart-item-details">
               <h3>${item.name}</h3>
               <div class="cart-quantity-control">
@@ -47,14 +57,14 @@ function updateCartItem() {
 
 }
 function removeItem(index){
-    cardNow.splice(index, 1)
+    cart.splice(index, 1)
     updateCartItem();
     updateCartBbadge()
 
 }
 function updateQuantity(index, change){
-    cardNow[index].quantity += change
-    if(cardNow[index].quantity <= 0){
+    cart[index].quantity += change
+    if(cart[index].quantity <= 0){
         removeItem(index)
     }
     else{
@@ -62,6 +72,6 @@ function updateQuantity(index, change){
     }
 }
 function updateTotalPrice(){
-    const totalPrice = cardNow.reduce((total, item) => total + item.price * item.quantity, 0);
+    const totalPrice = cart.reduce((total, item) => total + item.price * item.quantity, 0);
     total.textContent = totalPrice.toFixed(2);
 }
